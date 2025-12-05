e8de847aeaae2371dcbd7e12721b1db87dca60bb
felpsyor <felipemendes007@outlook.com>
2025-12-05 16:58:30 +0000

    adicionar scripts `lint:check`e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Atualização do projeto só pra ver se eu aprendi mesmo :P</h1>
+  return <h1>Atualização do projeto só pra ver se eu aprendi mesmo :P</h1>;
+}
+
+function test() {
+  console.log("test");
+}
+
+function test2() {
+  console.log("indentação errada");
 }
 
 export default Home;
